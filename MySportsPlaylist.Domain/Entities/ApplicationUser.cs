43d8f0aab7e3dc4933d1da695a4c356e84211535
Mohamed Maher <mohamedmaherali145@gmail.com>
2025-05-08 04:16:47 +0300

    Add Db Context

--- MySportsPlaylist.Domain/Entities/ApplicationUser.cs
+++ MySportsPlaylist.Domain/Entities/ApplicationUser.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
 
 namespace MySportsPlaylist.Domain.Entities
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser :IdentityUser<Guid>
     {
         public ICollection<Playlist> Playlists { get; set; } = new List<Playlist>();
     }
---
+++ MySportsPlaylist.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MySportsPlaylist.Domain.Entities;
+
+namespace MySportsPlaylist.Infrastructure.Data;
+public class ApplicationDbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+{
+    public DbSet<Match> Matches { get; set; }
+    public DbSet<Playlist> Playlists { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Playlist>().HasKey(p => new { p.UserId, p.MatchId });
+
+        builder.Entity<Playlist>()
+            .HasOne(p => p.User)
+            .WithMany(u => u.Playlists)
+            .HasForeignKey(p => p.UserId);
+
+        builder.Entity<Playlist>()
+            .HasOne(p => p.Match)
+            .WithMany()
+            .HasForeignKey(p => p.MatchId);
+    }
+}
+
